b69ba797aa3045d245566b40ef0da9d56d070fb6
saskiajasmin <122574944+saskiajasmin@users.noreply.github.com>
2023-04-11 19:03:13 +0200

    added basic css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <main>
+      <main className="panel">
         <Dictionary />
       </main>
       <footer className="text-center">
         <small>
           Open-Source Code{" "}
-          <a
-            href="https://github.com/saskiajasmin/react-dictionary"
-            target="_blank"
-            rel="noreferrer"
-            alt="GitHub">
-            @GitHub
-          </a>
+          <strong>
+            <a
+              href="https://github.com/saskiajasmin/react-dictionary"
+              target="_blank"
+              rel="noreferrer"
+              alt="GitHub">
+              @GitHub
+            </a>
+          </strong>
         </small>
       </footer>
     </div>
