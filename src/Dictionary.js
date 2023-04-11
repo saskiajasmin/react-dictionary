7892764b4a0c849a117394f9255166095fb4069c
saskiajasmin <122574944+saskiajasmin@users.noreply.github.com>
2023-04-12 01:05:26 +0200

    added photo api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,30 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handleImageResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let ImageApiKey = "b3b12fdbb6o3f95c54b073cata9173f5";
+    let ImageApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${ImageApiKey}`;
+
+    axios.get(ImageApiUrl).then(handleImageResponse);
   }
 
   function handleSubmit(event) {
@@ -48,6 +59,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
